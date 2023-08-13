c03b443481ffd821deeb407dfd8479d2b3ecde3a
Pavlo <yurchenkopavlo1@gmail.com>
2023-08-13 17:57:51 +0300

    github pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
-import {App} from "./App.tsx";
+import {App} from "./app/App.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
